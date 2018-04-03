695b042e9029a826a9b7f03038de912d5213745f
Claire <cconnachan@gmail.com>
2018-04-03 17:05:12 +0100

    added image carousel

--- app.rb
+++ app.rb
@@ -4,8 +4,10 @@ require('pry')
 require_relative('controllers/cyclists_controller')
 require_relative('controllers/rides_controller')
 require_relative('controllers/routes_controller')
+require_relative('models/image_carousel.rb')
 
 get '/' do
   @rides = Ride.ordered()
+  @routes = Route.all()
   erb(:index)
 end
---
+++ models/image_carousel.rb
@@ -0,0 +1,12 @@
+class ImageCarousel
+
+def self.random()
+  images = [
+    "/images/mull.jpg",
+    "/images/mull2.jpg",
+    "/images/arisaig.jpg"
+  ]
+return images.sample()
+end
+
+end
