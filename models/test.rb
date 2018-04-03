a80ce15e8f52321c128e29c75e5a81f76afa5161
Claire <cconnachan@gmail.com>
2018-04-03 16:04:42 +0100

    fixed loop error on front page, updated css

--- models/test.rb
+++ models/test.rb
@@ -27,3 +27,15 @@ p route.leaderboard()[1].get_cyclist.name
 p Route.longest()
 p Route.most_popular()
 p Route.highest()
+
+num = 0
+#while num < 5 do
+  Ride.all().each do |ride|
+
+    p ride.get_cyclist().name
+    p ride.get_route().title
+    p ride.date_logged
+    p num += 1
+    break if num == 5
+  end
+#end
