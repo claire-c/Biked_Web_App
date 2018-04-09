1fa4fc69d11a876910b1c41e3f706e37880df210
Claire <cconnachan@gmail.com>
2018-04-09 16:20:03 +0100

    updated db creds

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -5,9 +5,10 @@ class SqlRunner
   def self.run( sql, values = [] )
     begin
       # db = PG.connect({ dbname: 'cycling_app', host: 'localhost' })
-        db = PG.connect( {dbname: 'ddd1lpvalgmboj',
-host: 'ec2-54-243-210-70.compute-1.amazonaws.com',
-port: 5432, user: 'zkugusrihoaetl', password: 'f51dfcdb04f8b8675767100d4a07e0ff6ad40f48a7ce1548068f0cd4b8e84950'})
+#         db = PG.connect( {dbname: 'ddd1lpvalgmboj',
+# host: 'ec2-54-243-210-70.compute-1.amazonaws.com',
+# port: 5432, user: 'zkugusrihoaetl', password: 'f51dfcdb04f8b8675767100d4a07e0ff6ad40f48a7ce1548068f0cd4b8e84950'})
+db = PG.connect(ENV['DATABASE_URL'])
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
     ensure
