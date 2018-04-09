91dbfe56f2813b6bd2d340ba51373dd4ae3a9ecb
Claire <cconnachan@gmail.com>
2018-04-09 16:08:46 +0100

    fixed bug in db file

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -7,8 +7,7 @@ class SqlRunner
       # db = PG.connect({ dbname: 'cycling_app', host: 'localhost' })
         db = PG.connect( {dbname: 'ddd1lpvalgmboj',
 host: 'ec2-54-243-210-70.compute-1.amazonaws.com',
-port: 5432, user: 'zkugusrihoaetl', password: '
-f51dfcdb04f8b8675767100d4a07e0ff6ad40f48a7ce1548068f0cd4b8e84950'})
+port: 5432, user: 'zkugusrihoaetl', password: 'f51dfcdb04f8b8675767100d4a07e0ff6ad40f48a7ce1548068f0cd4b8e84950'})
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
     ensure
